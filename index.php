2bead18a429d885ebf085f4217b225d7be5d0055
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-05 14:44:39 -0400

    UI changes to sign up screen

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ unset($stmt);
 if(empty(trim($_POST["password"]))){
 $password_err = "Please enter a password.";
 } elseif(strlen(trim($_POST["password"])) < 6){
-$password_err = "Password must have atleast 6 characters.";
+$password_err = "Password must have at least 6 characters.";
 } else{
 $password = trim($_POST["password"]);
 }
@@ -97,38 +97,38 @@ unset($conn);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Theory // Sign Up</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px sans-serif; }
+        html { background-color: pink;}
+        body{ font: 14px 'Courier New'; }
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
 <body>
 <div class="wrapper">
-    <h2>Sign Up</h2>
-    <p>Please fill this form to create an account.</p>
+    <h2>create an account</h2>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group">
-            <label>Username</label>
+            <label>username</label>
             <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
             <span class="invalid-feedback"><?php echo $username_err; ?></span>
         </div>
         <div class="form-group">
-            <label>Password</label>
+            <label>password</label>
             <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
             <span class="invalid-feedback"><?php echo $password_err; ?></span>
         </div>
         <div class="form-group">
-            <label>Confirm Password</label>
+            <label>confirm password</label>
             <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
             <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            <input type="submit" class="btn btn-primary" value="submit" style="color: black">
+            <input type="reset" class="btn btn-secondary ml-2" value="reset" style="color: grey">
         </div>
-        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        <p>already have an account? <a href="login.php">login here</a>.</p>
     </form>
 </div>
 </body>
