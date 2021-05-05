b11aa023e2fcdaadd6b661ebc39bf32a27c903db
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-04 20:06:26 -0400

    Established DB connection with PHP

---
+++ src/main/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors'. 1);
+
+$servername = 'localhost';
+$username = "acano";
+$password = "V00873715";
+$database = "project_nguyenm26";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Connection failed: " . $exception->getMessage();
+}
+
+?>
