d4e60dcbc250e7d06d4a929ccb0e7c0137496dfa
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-07 17:29:04 -0400

    UI testing

--- welcome.php
+++ welcome.php
@@ -21,12 +21,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <h1 class="my-5">welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>!</h1>
 <h2>select one of the following:</h2>
 <p>
-    <a href="welcome.php" class="btn btn-light btn-lg btn-block">home</a>
+    <a href="welcome.php" class="btn btn-outline-secondary btn-lg btn-block">home</a>
     <a href="albums.php" class="btn btn-outline-info btn-lg btn-block">albums</a>
     <a href="artists.php" class="btn btn-outline-info btn-lg btn-block">artists</a>
     <a href="playlists.php" class="btn btn-outline-info btn-lg btn-block">playlists</a>
     <a href="songs.php" class="btn btn-outline-info btn-lg btn-block">songs</a>
-    <a href="logout.php" class="btn btn-light btn-lg btn-block">sign out</a>
+    <a href="logout.php" class="btn btn-outline-secondary btn-lg btn-block">sign out</a>
 </p>
 </body>
 </html>
