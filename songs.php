a1e7bc95da380011f4d0881e321d47b43e095a8a
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-07 18:05:13 -0400

    finally play button looks ok lol

--- songs.php
+++ songs.php
@@ -35,59 +35,49 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <div class="col">
             <img src="images/gods%20favorite%20band.jpg">
             <br>
-            <h2>boulevard of broken dreams</h2>
+            <h2>boulevard of broken dreams <img src="images/play.jpg"></h2>
             04:21
             <br>
             <a href="artists.php">green day</a>
             // <a href="albums.php"> green day: god's favorite band </a>
-            <br>
-            <img src="images/play%20button.jpg">
         </div>
         <div class="col">
             <img src="images/no%20need%20to%20argue%20album.jpg">
             <br>
-            <h2>zombie</h2>
+            <h2>zombie <img src="images/play.jpg"></h2>
             05:06
             <br>
             <a href="artists.php">the cranberries</a>
             // <a href="albums.php"> no need to argue</a>
-            <br>
-            <img src="images/play%20button.jpg">
         </div>
     </div>
     <div class="row">
         <div class="col">
             <img src="images/coarsegold.jpg">
             <br>
-            <h2>all the faces</h2>
+            <h2>all the faces <img src="images/play.jpg"></h2>
             02:59
             <br>
             <a href="artists.php">creed bratton</a>
             // <a href="albums.php"> coarsegold</a>
-            <br>
-            <img src="images/play%20button.jpg">
         </div>
         <div class="col">
             <img src="images/a%20rush%20of%20blood%20to%20the%20head.jpg">
             <br>
-            <h2>clocks</h2>
+            <h2>clocks <img src="images/play.jpg"></h2>
             05:07
             <br>
             <a href="artists.php">coldplay</a>
             // <a href="albums.php"> a rush of blood to the head</a>
-            <br>
-            <img src="images/play%20button.jpg">
         </div>
         <div class="col">
             <img src="images/bee%20gees%20greatest.jpg">
             <br>
-            <h2>stayin' alive</h2>
+            <h2>stayin' alive <img src="images/play.jpg"></h2>
             04:45
             <br>
             <a href="artists.php">bee gees</a>
             // <a href="albums.php"> bee gees' greatest hits</a>
-            <br>
-            <img src="images/play%20button.jpg">
         </div>
     </div>
 </div>
