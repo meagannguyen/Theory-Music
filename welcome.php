102302632f40702a8a4fd411b371b1c30cffa4b6
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-07 14:59:02 -0400

    start of adding music content

--- welcome.php
+++ welcome.php
@@ -16,13 +16,18 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <title>Welcome</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px 'Courier New'; text-align: center; }
+        body{ font: 20px 'Courier New'; text-align: center; }
     </style>
 </head>
 <body>
-<h1 class="my-5">hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>! welcome to theory :)</h1>
+<h1 class="my-5">hi, <b><?php echo htmlspecialchars($_SESSION["first_name"]); ?></b>!</h1>
+<h2>what are you in the mood for?</h2>
 <p>
-    <a href="reset-password.php" class="btn btn-warning">reset password</a>
+    <a href="albums.php" class="btn btn-outline-info">albums</a>
+    <a href="artists.php" class="btn btn-outline-info">artists</a>
+    <a href="genres.php" class="btn btn-outline-info">genres</a>
+    <a href="playlists.php" class="btn btn-outline-info">playlists</a>
+    <a href="songs.php" class="btn btn-outline-info">songs</a>
     <a href="logout.php" class="btn btn-danger ml-3">sign out</a>
 </p>
 </body>
