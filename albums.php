a54b53881b2880c16b572214b51fdfd38a81619d
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-07 16:12:27 -0400

    attempting UI bug fixes for albums

--- albums.php
+++ albums.php
@@ -27,35 +27,34 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <div class="col-sm">
                 <img src="images/some%20nights.jpg">
                 <a href="artists.php">fun.</a>
-                some nights
+                // some nights
             </div>
             <div class="col-sm">
                 <img src="images/gods%20favorite%20band.jpg">
                 <a href="artists.php">green day</a>
-                greatest hits: god's favorite band
+                // greatest hits: god's favorite band
             </div>
             <div class="col-sm">
                 <img src="images/no%20need%20to%20argue%20album.jpg">
-                <a href="artists.php">
-                    the cranberries</a>
-                no need to argue
+                <a href="artists.php">    the cranberries</a>
+                // no need to argue
             </div>
         </div>
         <div class="row">
             <div class="col-sm">
                 <img src="images/coarsegold.jpg">
                 <a href="artists.php">creed bratton</a>
-                coarsegold
+                // coarsegold
             </div>
             <div class="col-sm">
                 <img src="images/a%20rush%20of%20blood%20to%20the%20head.jpg">
                 <a href="artists.php">coldplay</a>
-                a rush of blood to the head
+                // a rush of blood to the head
             </div>
             <div class="col-sm">
                 <img src="images/bee%20gees%20greatest.jpg">
-                <a href="artists.php">
-                    bee gees</a>
+                <a href="artists.php">    bee gees</a>
+                // bee gees' greatest
             </div>
         </div>
     </div>
