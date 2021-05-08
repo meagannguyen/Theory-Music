dfb6a0778d5a53abeeeec8c30f7b914a45d0b4b7
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-08 16:51:16 -0400

    try { meagan's temper } catch { these hands }

--- playlists.php
+++ playlists.php
@@ -40,7 +40,7 @@ try {
     // $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account WHERE playlist.account = account.id");
     $currentUser = $_SESSION["username"];
     $sql = "SELECT ID FROM account WHERE username = :currentUser";
-    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account WHERE playlist.account = :sql");
+    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account WHERE playlist.account = $sql");
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
