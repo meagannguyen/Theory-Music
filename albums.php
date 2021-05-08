2f84b177c8f732e35fe7109c9adf4e7844658a92
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-08 14:07:20 -0400

    add home button + title to albums.php

--- albums.php
+++ albums.php
@@ -45,7 +45,7 @@ echo "</table>";
 
 
 
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -59,42 +59,6 @@ echo "</table>";
 <h1 class="my-5">albums</h1>
 <p>
     <a href="welcome.php" class="btn btn-light btn-lg btn-block">home</a>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm">
-                <img src="images/some%20nights.jpg">
-                <a href="artists.php">fun.</a>
-                // some nights
-            </div>
-            <div class="col-sm">
-                <img src="images/gods%20favorite%20band.jpg">
-                <a href="artists.php">green day</a>
-                // greatest hits: god's favorite band
-            </div>
-            <div class="col-sm">
-                <img src="images/no%20need%20to%20argue%20album.jpg">
-                <a href="artists.php">the cranberries</a>
-                // no need to argue
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm">
-                <img src="images/coarsegold.jpg">
-                <a href="artists.php">creed bratton</a>
-                // coarsegold
-            </div>
-            <div class="col-sm">
-                <img src="images/a%20rush%20of%20blood%20to%20the%20head.jpg">
-                <a href="artists.php">coldplay</a>
-                // a rush of blood to the head
-            </div>
-            <div class="col-sm">
-                <img src="images/bee%20gees%20greatest.jpg">
-                <a href="artists.php">bee gees</a>
-                // bee gees' greatest
-            </div>
-        </div>
-    </div>
 </p>
 </body>
 </html> -->
