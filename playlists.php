6c98a013d930c9aa55a4aa9a59a66792a300d095
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 09:45:47 -0400

    attempting to show playlists pt a million

--- playlists.php
+++ playlists.php
@@ -40,7 +40,7 @@ try {
     // $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account WHERE playlist.account = account.id");
     $currentUser = $_SESSION["username"];
     $sql = $conn->prepare("SELECT ID FROM account WHERE username = :currentUser");
-    $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
+    // $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
     $sql->execute();
     $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :currentUser");
     $stmt->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
