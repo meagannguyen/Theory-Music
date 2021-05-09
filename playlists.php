4b1d23999b9637857ba30e153d5775ee2927f39a
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 14:33:50 -0400

    this is an absolute pain

--- playlists.php
+++ playlists.php
@@ -43,9 +43,9 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $currentUser = $_SESSION["username"];
-    $sql = $conn->prepare("SELECT ID FROM account WHERE username = :currentUser");
-    $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
-    $sql->execute();
+    $sql = $conn->prepare("SELECT ID FROM account WHERE username = $currentUser");
+    //$sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
+    //$sql->execute();
     console_log( $sql );
     $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :sql");
     $stmt->bindParam(':sql', $sql, PDO::PARAM_STR);
