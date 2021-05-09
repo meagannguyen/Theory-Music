41ee4dd965a2c6c5acbb771916d4db7a45b94728
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 09:30:08 -0400

    f

--- playlists.php
+++ playlists.php
@@ -41,7 +41,7 @@ try {
     $currentUser = $_SESSION["username"];
     /*$sql = $conn->prepare("SELECT ID FROM account WHERE username = :currentUser");
     $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);*/
-    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account ON playlist.account = :currentUser");
+    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :currentUser");
     $stmt->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
