407057a536fcc41d6f5f43765ee8c3976f817d06
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 14:43:24 -0400

    attempting solution #437925345

--- playlists.php
+++ playlists.php
@@ -44,11 +44,11 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $currentUser = $_SESSION["username"];
     $sql = $conn->prepare("SELECT ID FROM account WHERE username = $currentUser");
-    //$sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
-    //$sql->execute();
     console_log( $sql );
-    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :sql");
-    $stmt->bindParam(':sql', $sql, PDO::PARAM_STR);
+    $sql->execute();
+    $result = $sql->fetchAll(PDO::FETCH_OBJ);
+    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :result");
+    $stmt->bindParam(':result', $result, PDO::PARAM_STR);
     $stmt->execute();
 //    $sql = "SELECT ID FROM account WHERE username = :currentUser";
 //    $stmt = $conn->prepare($sql);
