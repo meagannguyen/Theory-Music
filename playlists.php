ffd351a570263bb059bdc1b59d7578fa820e45c3
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 09:50:37 -0400

    reverting changes to get rid of error

--- playlists.php
+++ playlists.php
@@ -40,9 +40,9 @@ try {
     // $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account WHERE playlist.account = account.id");
     $currentUser = $_SESSION["username"];
     $sql = $conn->prepare("SELECT ID FROM account WHERE username = $currentUser");
-    // $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
-    // $sql->execute();
-    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = $currentUser");
+    $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
+    $sql->execute();
+    $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :currentUser");
     $stmt->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
