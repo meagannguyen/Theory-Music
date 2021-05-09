6a22e7c93658fd31a101f39efb9eafdb66e94e7f
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 09:17:26 -0400

    bug fix oops

--- playlists.php
+++ playlists.php
@@ -43,7 +43,6 @@ try {
     $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
     $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist JOIN account WHERE playlist.account = :sql");
     $stmt->bindParam(':sql', $sql, PDO::PARAM_STR);
-    $sql->execute();
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
