a42f61b2cdf1897b5e5db9cc378e690998ae5ff4
Meagan Nguyen <nguyenm26@vcu.edu>
2021-05-09 13:28:32 -0400

    debuggin pt 3

--- playlists.php
+++ playlists.php
@@ -39,7 +39,8 @@ try {
     $currentUser = $_SESSION["username"];
     $sql = $conn->prepare("SELECT ID FROM account WHERE username = :currentUser");
     $sql->bindParam(':currentUser', $currentUser, PDO::PARAM_STR);
-    // $sql->execute();
+    $sql->execute();
+    echo $sql;
     $stmt = $conn->prepare("SELECT playlist.name AS playlist, playlist.date_created AS 'date created', playlist.duration AS duration, playlist.num_followers AS followers FROM playlist WHERE playlist.account = :sql");
     $stmt->bindParam(':sql', $sql, PDO::PARAM_STR);
     $stmt->execute();
